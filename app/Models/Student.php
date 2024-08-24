ca1ca83c0eee29b584160fceee93f8d8b2dbbf00
VanQuan0174 <quanv8786@gmail.com>
2024-08-24 22:13:20 +0700

    student room dayofweek 24/8

---
+++ app/Models/Student.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
+class Student extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $table = 'students';
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'birth_day',
+        'address',
+        'enrollment_date',
+    ];
+}
