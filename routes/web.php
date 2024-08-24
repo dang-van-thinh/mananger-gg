2e73003837856a842948406adc7e02f19a51a597
VanQuan0174 <quanv8786@gmail.com>
2024-08-24 21:22:36 +0700

    quan check git

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
+// check git
 */
 
 Route::get('/', function () {
