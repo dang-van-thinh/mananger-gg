ef647f7fa765da98798663aee5c130d6dab1e211
QuanNVph3649 <quanv8786@gmail.com>
2024-09-03 22:35:29 +0700

    cập nhật left-panel ca học

--- resources/views/layouts/partials/left-panel.blade.php
+++ resources/views/layouts/partials/left-panel.blade.php
@@ -45,6 +45,14 @@
                         <li><i class="fa fa-list"></i><a href="{{ route('rooms.index') }}">Danh sách phòng học</a></li>
                     </ul>
                 </li>
+                
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false"><i class="menu-icon fa fa-clock-o"></i>QL ca học</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-list"></i><a href="{{ route('sessions.index') }}">Danh sách ca học</a></li>
+                    </ul>
+                </li>
 
             </ul>
         </div>
